b05769dbfa7ada8b04caa1f56df679c5e56e75e4
KorolevskiTigr2 <mega.medved87gmail.com>
2023-01-09 02:58:51 +0300

    add a new todo...

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const genereateTemplate = todo => {
+ 
+    const html = `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far a-trash-alt delete"></i>
+      </li>   
+    `
+    list.innerHTML += html; 
+; }
+
+
+
+addForm.addEventListener('submit', e => {
+  
+ e.preventDefault();
+ const todo = addForm.add.value.trim();
+ 
+ if(todo.length) {
+    genereateTemplate(todo);
+    addForm.reset();  
+ }
+
+});
